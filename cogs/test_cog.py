650d1cedd783ba22c9217f6fa446982cc4ee479d
Marmo77 <matyskamela@gmail.com>
2025-11-26 23:02:42 +0100

    Cogs functions

---
+++ cogs/test_cog.py
@@ -0,0 +1,6 @@
+def information():
+    print("------------")
+    print("The project is a Discord bot that can be used by anyone on discord.")
+    print("Bot will have variable functions and can be expanded in the future.")
+    print("This is open-source project and can be used by anyone. Created for expanding my knowledge of python and nextcord even further.")
+    print("------------")
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import random
+
+from cogs.test_cog import information
+
 print("Hello World")
 print(f"Your lucky number today is: {random.randint(1,10)}")
+
+information()
